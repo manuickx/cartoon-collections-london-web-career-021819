43829537e51e3b69497ae8bf8cd414f17b515b3c
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-05 21:35:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,10 @@ def long_planeteer_calls(array)
 end
 end
 
-def find_the_cheese(array)
-  cheese = ["cheddar", "gouda"]
-  array.find do |food|
-    chese.include(food)
-end
-end
   
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |food|
+    cheese_types.include?(food)
+    end
 end
